192c11a3c3bae063901257b8646dc594cb397a39
SusanBeno <beno.susan@gmail.com>
2021-06-22 07:45:28 -0400

    pseudocode forms

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+//email input form 
+var firstName = document.getElementById("first-name");
+var lastName = document.getElementById("last-name");
+var email = document.getElementById("email");
+var submitButton = document.getElementById("submit-button")
+
+// on keystroke - name input- if entry is letters and spaces no numbers or special characters, display green checkbox; else display red x
+
+// on keystroke email input - if entry is valid email display green check else display red x 
+
+// on click - if first name and last name and email are valid allow submit
