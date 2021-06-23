a60a08ffb69e7364f82aa778888575c6d91d184e
SusanBeno <beno.susan@gmail.com>
2021-06-23 13:00:05 -0400

    testing regex logic

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-
-
-//email input form 
+//email input form
 var firstName = document.getElementById("first-name");
 var lastName = document.getElementById("last-name");
 var email = document.getElementById("email");
-var submitButton = document.getElementById("submit-button")
+var submitButton = document.getElementById("submit-button");
 
 // on keystroke - name input- if entry is letters and spaces no numbers or special characters, display green checkbox; else display red x
 
-// on keystroke email input - if entry is valid email display green check else display red x 
+function checkletters() {
+  var reg = /^[a-z]+$/i;
+  console.log(reg);
+  console.log(reg.test("something"));
+
+  var testName = "test2";
+
+  if (testName.match(reg)) {
+    console.log("success");
+  } else {
+    console.log("try again!");
+  }
+}
+checkletters()
+// on keystroke email input - if entry is valid email display green check else display red x
 
 // on click - if first name and last name and email are valid allow submit
